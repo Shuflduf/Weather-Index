13c532f009423b909dff78c67a05bad4119ba724
Shuflduf <shuflduf@gmail.com>
2026-06-27 02:29:23 -0600

    switch from rocket to axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,11 @@
-#[macro_use]
-extern crate rocket;
+use std::{net::SocketAddr, sync::Arc};
 
-use rocket::{serde::json::Json, State};
-use sea_orm::{ActiveModelTrait, DatabaseConnection};
+use axum::{
+    extract::State,
+    routing::{get, post},
+    Json, Router,
+};
+use sea_orm::{sea_query::prelude::serde_json, ActiveModelTrait, DatabaseConnection};
 
 use crate::{entity::run_report, run_report_dto::RunReportDTO};
 
@@ -10,27 +13,29 @@ mod db;
 mod entity;
 mod run_report_dto;
 
-#[get("/")]
-async fn index(_conn: &State<DatabaseConnection>) -> String {
-    "hello".into()
-}
-
-#[post("/new-run", data = "<data>")]
-async fn create_game_run(db: &State<DatabaseConnection>, data: Json<RunReportDTO>) -> String {
-    let game_run: run_report::ActiveModel = data.into_inner().try_into().unwrap();
-    match game_run.insert(db.inner()).await {
-        Ok(_) => "Game run created!".into(),
-        Err(e) => format!("Error: {}", e),
-    }
-}
-
-#[launch]
-async fn rocket() -> _ {
+#[tokio::main]
+async fn main() {
     dotenvy::dotenv().ok();
 
     let db = db::init_db().await.expect("Failed to initialize database");
+    let state = Arc::new(db);
 
-    rocket::build()
-        .manage(db)
-        .mount("/", routes![index, create_game_run])
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/new-run", post(insert_new_run))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn insert_new_run(
+    State(db): State<Arc<DatabaseConnection>>,
+    Json(payload): Json<RunReportDTO>,
+) -> String {
+    let game_run: run_report::ActiveModel = payload.try_into().unwrap();
+    match game_run.insert(&*db).await {
+        Ok(_) => "Game run created!".into(),
+        Err(e) => format!("Error: {}", e),
+    }
 }
