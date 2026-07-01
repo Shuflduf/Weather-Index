5aea4f7690c27014d176defd66108c55bd303e0b
Shuflduf <shuflduf@gmail.com>
2026-06-30 22:44:55 -0600

    login thing

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { SiGithub } from "@icons-pack/svelte-simple-icons";
+  import { onMount } from "svelte";
 
-  let loggedIn = $state(false);
+  let user: any = $state(null);
+
   async function startGithubOauth() {
     let resp = await fetch("/auth/sign-in/social", {
       method: "POST",
@@ -11,6 +13,17 @@
     let { url } = await resp.json();
     if (url) window.location.href = url;
   }
+
+  onMount(async () => {
+    let resp = await fetch("/auth/get-session");
+    let body = await resp.json();
+    if (body.user) {
+      user = body.user;
+      console.log(user);
+    } else {
+      user = false;
+    }
+  });
 </script>
 
 <div
@@ -19,12 +32,19 @@
   <h1 class="text-primary text-2xl font-bold tracking-tighter pl-4">
     Weather Index
   </h1>
-  {#if loggedIn}
-    sdoifjskdf
+  {#if user}
+    <div
+      class="flex flex-row h-full justify-center items-center gap-2 px-4 border-l"
+    >
+      <img src={user.image} alt="profile" class="h-4/5 rounded-full" />
+      <span class="text-primary text-xl"
+        >{user.displayName ?? user.username}</span
+      >
+    </div>
   {:else}
     <button
-      class="flex bg-bg-secondary hover:bg-default transition flex-row gap-2 text-xl border-l px-4 h-full text-primary justify-center items-center cursor-pointer"
-      onclick={startGithubOauth}
+      class={`flex bg-bg-secondary hover:bg-default flex-row gap-2 text-xl border-l px-4 h-full text-primary transition justify-center items-center ${user == false ? "cursor-pointer" : "cursor-not-allowed"}`}
+      onclick={user == false ? startGithubOauth : null}
     >
       <SiGithub />
       <span>Log In</span>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,6 +5,6 @@
 <Navbar />
 <h1>Welcome to SvelteKit</h1>
 <p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
-  the documentation
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
 </p>
