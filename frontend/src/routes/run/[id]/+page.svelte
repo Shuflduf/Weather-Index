c9dc3698c1934d72ffb62798688546d8a051aecf
Shuflduf <shuflduf@gmail.com>
2026-07-04 20:27:44 -0600

    item display

--- frontend/src/routes/run/[id]/+page.svelte
+++ frontend/src/routes/run/[id]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import { ITEMS } from "$lib/Items";
   import { onMount } from "svelte";
 
-  let run = $state();
+  let run: any = $state();
 
   onMount(async () => {
     const runId = page.params.id;
@@ -10,3 +11,27 @@
     console.log(run);
   });
 </script>
+
+<div class="text-primary">
+  {#if run}
+    <ul class="flex flex-row">
+      {#each Object.entries(run.items) as [itemId, itemCount]}
+        {@const item = ITEMS[Number(itemId)]}
+        {#if !item.helper}
+          <li class="relative">
+            <img
+              src={`/items/${item.icon}`}
+              alt={item.displayName}
+              class="w-16 inline"
+            />
+            <p
+              class="font-ror2 text-2xl absolute top-0 right-0 text-shadow-lg/50"
+            >
+              x{itemCount}
+            </p>
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  {/if}
+</div>
