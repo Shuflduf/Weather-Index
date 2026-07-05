a6b9638641f72815daea9dca3fede5aa3ea80d9c
Shuflduf <shuflduf@gmail.com>
2026-07-05 05:05:50 -0600

    slowly adding stats

--- frontend/src/lib/RoR2.ts
+++ frontend/src/lib/RoR2.ts
@@ -2,6 +2,7 @@ import bodies from "$lib/bodies.json";
 import difficulties from "$lib/difficulties.json";
 import endings from "$lib/endings.json";
 import items from "$lib/items.json";
+import scoring_table from "$lib/scoring.json";
 
 export type Item = {
   id: number;
@@ -36,16 +37,42 @@ export type Difficulty = {
   icon: string;
 };
 
+export type ScoringTable = {
+  timeAliveSeconds: number;
+  kills: number;
+  minionKills: number;
+  damageDealt: number;
+  minionDamageDealt: number;
+  highestDamageDealt: number;
+  highestLevel: number;
+  goldCollected: number;
+  itemsCollected: number;
+  stagesCompleted: number;
+  purchases: number;
+};
+
 export const ITEMS = Object.fromEntries(items.map((item: Item) => [item.id, item]));
 export const BODIES = Object.fromEntries(bodies.map((body: Body) => [body.name, body]));
 export const ENDINGS = Object.fromEntries(endings.map((ending: Ending) => [ending.name, ending]));
 export const DIFFICULTIES = Object.fromEntries(
   difficulties.map((difficulty: Difficulty) => [difficulty.nameToken, difficulty]),
 );
+export const SCORING_TABLE = scoring_table as ScoringTable;
 
 export function countRealItems(items: Record<string, number>): number {
   return Object.entries(items).filter(([id, _]) => !ITEMS[id].helper).reduce(
     (accum, [_, current]) => accum + current,
     0,
   );
+}
+
+export function formatSeconds(seconds: number): string {
+  const date = new Date(null);
+  date.setSeconds(seconds);
+  const result = date.toISOString().slice(11, 19);
+  return result;
+}
+
+export function formatBig(big: number): string {
+  return big.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
--- frontend/src/routes/run/[id]/+page.svelte
+++ frontend/src/routes/run/[id]/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
   import { page } from "$app/state";
-  import { ENDINGS, ITEMS } from "$lib/RoR2";
+  import {
+    DIFFICULTIES,
+    ENDINGS,
+    formatBig,
+    formatSeconds,
+    ITEMS,
+    SCORING_TABLE,
+  } from "$lib/RoR2";
   import { onMount } from "svelte";
 
   let runPromise: Promise<any> = $state(new Promise(() => {}));
@@ -17,7 +24,7 @@
   {:then run}
     <div
       style={`background-color: ${ENDINGS[run.ending].colorBg};`}
-      class="m-4 border"
+      class="m-4 border mb-0"
     >
       <img
         src={`/endings/${ENDINGS[run.ending].icon}`}
@@ -28,6 +35,52 @@
       <h1 class="inline text-4xl align-middle tracking-wide italic font-medium">
         {ENDINGS[run.ending].endingMessage}
       </h1>
+    </div>
+    <div class="flex flex-row gap-4 m-4">
+      <div class="border bg-bg-secondary w-full p-2">
+        <h1 class="text-3xl text-center">Stats</h1>
+        <div class="flex flex-row justify-between items-center p-2">
+          <span class="text-lg">
+            {DIFFICULTIES[run.difficulty].displayName}
+          </span>
+          <img
+            src={`/difficulties/${DIFFICULTIES[run.difficulty].icon}`}
+            alt={run.difficulty}
+            class="h-12 inline mr-2"
+          />
+        </div>
+        <div class="flex flex-row justify-between items-center p-2">
+          <span>
+            Time Alive: <span class="text-yellow-200">
+              {formatSeconds(run.time_alive_seconds)}
+            </span>
+          </span>
+          <span>
+            <span class="text-yellow-200">
+              {formatBig(
+                run.time_alive_seconds * SCORING_TABLE.timeAliveSeconds,
+              )}
+            </span>
+            pts
+          </span>
+        </div>
+        <div class="flex flex-row justify-between items-center p-2">
+          <span>
+            Kills: <span class="text-yellow-200">
+              {formatBig(run.kills)}
+            </span>
+          </span>
+          <span>
+            <span class="text-yellow-200">
+              {formatBig(run.kills * SCORING_TABLE.kills)}
+            </span>
+            pts
+          </span>
+        </div>
+      </div>
+      <div class="bg-red-500 w-full">
+        <h1>Info</h1>
+      </div>
     </div>
     <ul class="flex flex-row">
       {#each Object.entries(run.items) as [itemId, itemCount]}
