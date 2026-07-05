6ef3decc80a2c72570baef4b838609ca8f3172a5
Shuflduf <shuflduf@gmail.com>
2026-07-05 04:46:35 -0600

    starting run page

--- frontend/src/routes/run/[id]/+page.svelte
+++ frontend/src/routes/run/[id]/+page.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
   import { page } from "$app/state";
-  import { ITEMS } from "$lib/RoR2";
+  import { ENDINGS, ITEMS } from "$lib/RoR2";
   import { onMount } from "svelte";
 
-  let run: any = $state();
+  let runPromise: Promise<any> = $state(new Promise(() => {}));
 
   onMount(async () => {
     const runId = page.params.id;
-    run = await (await fetch(`/api/runs/${runId}`)).json();
-    console.log(run);
+    runPromise = fetch(`/api/runs/${runId}`).then((resp) => resp.json());
   });
 </script>
 
 <div class="text-primary">
-  {#if run}
+  {#await runPromise}
+    waiting
+  {:then run}
+    <div
+      style={`background-color: ${ENDINGS[run.ending].colorBg};`}
+      class="m-4 border"
+    >
+      <img
+        src={`/endings/${ENDINGS[run.ending].icon}`}
+        class="h-24 inline cursor-help"
+        alt={ENDINGS[run.ending].name}
+        title={ENDINGS[run.ending].name}
+      />
+      <h1 class="inline text-4xl align-middle tracking-wide italic font-medium">
+        {ENDINGS[run.ending].endingMessage}
+      </h1>
+    </div>
     <ul class="flex flex-row">
       {#each Object.entries(run.items) as [itemId, itemCount]}
         {@const item = ITEMS[Number(itemId)]}
@@ -23,6 +38,7 @@
               src={`/items/${item.icon}`}
               alt={item.displayName}
               class="w-16 inline"
+              title={item.displayName}
             />
             <p
               class="font-ror2 text-2xl absolute top-0 right-0 text-shadow-lg/50"
@@ -33,5 +49,7 @@
         {/if}
       {/each}
     </ul>
-  {/if}
+  {:catch err}
+    {err}
+  {/await}
 </div>
