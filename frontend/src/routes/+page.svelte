30638ec13f7dea416104851a967363c9e5da1ff3
Shuflduf <shuflduf@gmail.com>
2026-07-07 14:38:56 -0600

    icons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { ArrowUpNarrowWide, ArrowDownNarrowWide } from "@lucide/svelte";
   import { onMount, tick } from "svelte";
   import {
     BODIES,
@@ -452,7 +453,7 @@
   tabindex="-1"
   id="context-menu"
   popover
-  class="fixed bg-bg-secondary border text-primary p-2 w-32"
+  class="fixed bg-bg-secondary border text-primary p-2 w-40"
   style="left: {contextMenu.pos[0]}px; top: {contextMenu.pos[1]}px;"
   oncontextmenu={(e) => e.preventDefault()}
 >
@@ -467,26 +468,26 @@
         bind:checked={properties[contextMenu.id].enabled}
       />
     </div>
-    <button
-      class={`border w-full p-2 cursor-pointer transition-colors ${
-        sortProperty.by == contextMenu.id && sortProperty.sort == "ASC"
-          ? "bg-active"
-          : "bg-default hover:bg-hover "
-      }`}
-      onclick={() => setSort("ASC")}
-    >
-      Ascending
-    </button>
-    <button
-      class={`border w-full p-2 cursor-pointer transition-colors ${
-        sortProperty.by == contextMenu.id && sortProperty.sort == "DESC"
-          ? "bg-active"
-          : "bg-default hover:bg-hover "
-      }`}
-      onclick={() => setSort("DESC")}
-    >
-      Descending
-    </button>
+    {#snippet sortButton(sort: SortMode)}
+      <button
+        class={`border w-full p-2 cursor-pointer transition-colors flex flex-row gap-4 ${
+          sortProperty.by == contextMenu.id && sortProperty.sort == sort
+            ? "bg-active"
+            : "bg-default hover:bg-hover "
+        }`}
+        onclick={() => setSort(sort)}
+      >
+        {#if sort == "ASC"}
+          <ArrowUpNarrowWide />
+          <span>Ascending</span>
+        {:else}
+          <ArrowDownNarrowWide />
+          <span>Descending</span>
+        {/if}
+      </button>
+    {/snippet}
+    {@render sortButton("ASC")}
+    {@render sortButton("DESC")}
   {/if}
 </div>
 
