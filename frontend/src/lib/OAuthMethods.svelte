f4475b6686787fc48a1e525c3fc00c14a74e633b
Shuflduf <shuflduf@gmail.com>
2026-07-13 05:13:27 -0600

    sign up

---
+++ frontend/src/lib/OAuthMethods.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import {
+    SiDiscord,
+    SiGithub,
+    SiGoogle,
+  } from "@icons-pack/svelte-simple-icons";
+
+  async function startOauth(provider: string) {
+    let resp = await fetch("/auth/sign-in/social", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ provider: provider }),
+    });
+    let { url } = await resp.json();
+    if (url) window.location.href = url;
+  }
+</script>
+
+<h2 class="text-2xl tracking-tighter font-bold mt-8">Alternative Methods</h2>
+<div class="mt-4 flex flex-row gap-4">
+  <button
+    onclick={() => startOauth("github")}
+    class="p-2 bg-default hover:bg-hover active:bg-active transition-colors border cursor-pointer"
+  >
+    <SiGithub title="GitHub" />
+  </button>
+  <button
+    onclick={() => startOauth("google")}
+    class="p-2 bg-default hover:bg-hover active:bg-active transition-colors border cursor-pointer"
+  >
+    <SiGoogle title="Google" />
+  </button>
+  <button
+    onclick={() => startOauth("discord")}
+    class="p-2 bg-default hover:bg-hover active:bg-active transition-colors border cursor-pointer"
+  >
+    <SiDiscord title="Discord" />
+  </button>
+</div>
--- frontend/src/routes/sign-up/+page.svelte
+++ frontend/src/routes/sign-up/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-  import { Mail, KeyRound, Heart } from "@lucide/svelte";
+  import { Mail, KeyRound, Heart, User, Pen } from "@lucide/svelte";
   import OAuthMethods from "$lib/OAuthMethods.svelte";
 
+  let username: string = $state("");
+  let displayUsername: string = $state("");
   let email: string = $state("");
   let password: string = $state("");
 
   async function signIn() {
-    const resp = await fetch("/auth/sign-in/email", {
+    const resp = await fetch("/auth/sign-up/email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        name: username,
+        username,
+        displayUsername,
         email,
         password,
       }),
@@ -20,7 +25,7 @@
 </script>
 
 <div class="flex flex-col items-center">
-  <h1 class="text-3xl tracking-tighter font-bold">Sign In</h1>
+  <h1 class="text-3xl tracking-tighter font-bold">Sign Up</h1>
   <div class="flex flex-col w-80">
     <label
       for="email"
@@ -44,6 +49,38 @@
       type="password"
       id="password"
       bind:value={password}
+      class="p-2 bg-default hover:bg-hover active:bg-active transition-colors border outline-none font-mono"
+    />
+    <label
+      for="username"
+      class="text-xl tracking-tighter mt-4 flex flex-row gap-1 items-center font-mono"
+    >
+      <User /> Username
+    </label>
+    <div
+      class=" bg-default hover:bg-hover active:bg-active border transition-colors flex flex-row"
+    >
+      <span class="p-2 pr-0">@</span>
+      <input
+        type="text"
+        autocomplete="username"
+        id="username"
+        bind:value={username}
+        class="outline-none font-mono w-full p-2 pl-0"
+      />
+    </div>
+    <label
+      for="displayUsername"
+      class="text-xl tracking-tighter mt-4 flex flex-row gap-1 items-center font-mono"
+    >
+      <Pen /> Display Username
+    </label>
+    <input
+      type="text"
+      autocomplete="nickname"
+      id="displayUsername"
+      placeholder={username}
+      bind:value={displayUsername}
       class="p-2 bg-default hover:bg-hover active:bg-active transition-colors border outline-none font-mono"
     />
     <button
