6c4b8abb3ebaf68930aea964f0400c9c33bc4e63
Shuflduf <shuflduf@gmail.com>
2026-07-27 21:35:45 -0600

    wtf

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -15,7 +15,7 @@ pub fn router() -> Router<Arc<WIState>> {
     Router::new()
         .route("/runs", get(runs::list::get))
         .route("/runs/{id}", get(runs::get))
-        .route("/runs/new", get(runs::new::post))
+        .route("/runs/new", post(runs::new::post))
         .route("/player/{username}", get(player::get))
         .route("/player", post(player::update))
         .route("/stats/sum", get(stats::combined::sum))
