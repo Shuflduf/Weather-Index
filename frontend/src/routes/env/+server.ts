9be2f7169bf2fcf33873db29509f6e15e4ef511f
Shuflduf <shuflduf@gmail.com>
2026-08-04 05:43:45 -0600

    oh oops

--- frontend/src/routes/env/+server.ts
+++ frontend/src/routes/env/+server.ts
@@ -2,5 +2,14 @@ import type { RequestHandler } from './$types';
 import { env } from '$env/dynamic/private';
 
 export const GET: RequestHandler = () => {
-  return new Response(JSON.stringify(env));
+  const allowedKeys = ["VERCEL", "VERCEL_GIT_COMMIT_SHA"]
+  return new Response(
+    JSON.stringify(
+      Object.fromEntries(
+        Object.entries(env).filter(
+          ([key, _]) => allowedKeys.includes(key)
+        )
+      )
+    )
+  );
 };
