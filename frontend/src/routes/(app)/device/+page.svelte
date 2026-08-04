702ffc10376304c7dd65d1bbf3f788550dd262d0
Shuflduf <shuflduf@gmail.com>
2026-08-03 22:15:56 -0600

    better device page

--- frontend/src/routes/(app)/device/+page.svelte
+++ frontend/src/routes/(app)/device/+page.svelte
@@ -3,7 +3,12 @@
   import { page } from "$app/state";
   import { auth } from "$lib";
   import { authedFetch } from "$lib/auth";
+  import LoadingIndicator from "$lib/LoadingIndicator.svelte";
 
+  let currentUserPromise: Promise<{
+    session: any;
+    user: { username: string };
+  }> = $state(new Promise(() => {}));
   let userCode = $state("");
   let status: "PENDING" | "APPROVED" | "ERROR" | "DENIED" = $state("PENDING");
 
@@ -12,6 +17,7 @@
     if (params.has("user_code")) {
       userCode = params.get("user_code")!;
     }
+    currentUserPromise = authedFetch(auth("get-session")).then((r) => r.json());
   });
 
   async function approve() {
@@ -44,22 +50,29 @@
   }
 </script>
 
-<div class="flex justify-center items-center flex-col">
-  <span class="text-bold text-3xl tracking-tighter">
-    {status}
-  </span>
-  <div class="mt-4">
-    <button
-      class="bg-default active:bg-active transition p-2 cursor-pointer hover:bg-hover border"
-      onclick={approve}
-    >
-      Connect
-    </button>
-    <button
-      class="bg-default active:bg-active transition p-2 cursor-pointer hover:bg-hover border"
-      onclick={deny}
-    >
-      Cancel
-    </button>
+{#await currentUserPromise}
+  <LoadingIndicator text="Loading user" indicator />
+{:then currentUser}
+  <div class="flex justify-center items-center flex-col">
+    <span>
+      Connecting as <b>@{currentUser.user.username}</b>
+    </span>
+    <span class="text-bold text-3xl tracking-tighter">
+      {status}
+    </span>
+    <div class="mt-4">
+      <button
+        class="bg-default active:bg-active transition p-2 cursor-pointer hover:bg-hover border"
+        onclick={approve}
+      >
+        Connect
+      </button>
+      <button
+        class="bg-default active:bg-active transition p-2 cursor-pointer hover:bg-hover border"
+        onclick={deny}
+      >
+        Cancel
+      </button>
+    </div>
   </div>
-</div>
+{/await}
